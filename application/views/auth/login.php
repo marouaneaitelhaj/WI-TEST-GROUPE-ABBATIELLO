dd22eba505cde59fe1347add13994f73c0f80038
MAROUANOX AIT ELHAJ <marwaneaitelhaj001@gmail.com>
2024-12-17 22:22:08 +0100

    Refactor login view to use base_url for asset paths and update form action for authentication

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -4,36 +4,41 @@
     <meta charset="UTF-8">
     <title>AdminLTE 2 | Log in</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-    <!-- Bootstrap 3.3.2 -->
-    <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <!-- Font Awesome Icons -->
+
+    <!-- Bootstrap 3.3.7 -->
+    <link href="<?= base_url('assets/adminlte/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css" />
+    <!-- Font Awesome -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
-    <link href="../../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?= base_url('assets/adminlte/dist/css/AdminLTE.min.css'); ?>" rel="stylesheet" type="text/css" />
     <!-- iCheck -->
-    <link href="../../plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
+    <link href="<?= base_url('assets/adminlte/plugins/iCheck/square/blue.css'); ?>" rel="stylesheet" type="text/css" />
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!-- HTML5 Shim and Respond.js IE8 support -->
     <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
   </head>
   <body class="login-page">
     <div class="login-box">
       <div class="login-logo">
-        <a href="../../index2.html"><b>Admin</b>LTE</a>
+        <a href="<?= base_url('index.php/admin'); ?>"><b>Admin</b>LTE</a>
       </div><!-- /.login-logo -->
+
       <div class="login-box-body">
         <p class="login-box-msg">Sign in to start your session</p>
-        <form action="../../index2.html" method="post">
+        <?php if ($this->session->flashdata('error')): ?>
+            <p style="color: red;"><?php echo $this->session->flashdata('error'); ?></p>
+        <?php endif; ?>
+
+        <form action="<?php echo site_url('auth/do_login'); ?>" method="post">
           <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Email"/>
+            <input type="text" name="login" class="form-control" placeholder="Login" />
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
           </div>
           <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password"/>
+            <input type="password" name="mot_de_passe" class="form-control" placeholder="Password" />
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
           </div>
           <div class="row">
@@ -50,24 +55,17 @@
           </div>
         </form>
 
-        <div class="social-auth-links text-center">
-          <p>- OR -</p>
-          <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
-          <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
-        </div><!-- /.social-auth-links -->
-
-        <a href="#">I forgot my password</a><br>
-        <a href="register.html" class="text-center">Register a new membership</a>
-
+        <a href="<?php echo site_url('auth/register'); ?>" class="text-center">Register</a>
       </div><!-- /.login-box-body -->
     </div><!-- /.login-box -->
 
     <!-- jQuery 2.1.3 -->
-    <script src="../../plugins/jQuery/jQuery-2.1.3.min.js"></script>
-    <!-- Bootstrap 3.3.2 JS -->
-    <script src="../../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="<?= base_url('assets/adminlte/plugins/jQuery/jQuery-2.1.3.min.js'); ?>"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="<?= base_url('assets/adminlte/bootstrap/js/bootstrap.min.js'); ?>" type="text/javascript"></script>
     <!-- iCheck -->
-    <script src="../../plugins/iCheck/icheck.min.js" type="text/javascript"></script>
+    <script src="<?= base_url('assets/adminlte/plugins/iCheck/icheck.min.js'); ?>" type="text/javascript"></script>
+
     <script>
       $(function () {
         $('input').iCheck({
