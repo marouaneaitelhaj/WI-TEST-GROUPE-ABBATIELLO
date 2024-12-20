40ae4494e668858ea05a98f48ff5a446ebe91239
MAROUANOX AIT ELHAJ <marwaneaitelhaj001@gmail.com>
2024-12-20 18:39:57 +0100

    Add jQuery for dynamic user search and sorting; refactor search form and table headers

--- application/views/utilisateurDashboard/index.php
+++ application/views/utilisateurDashboard/index.php
@@ -28,26 +28,24 @@
             </a>
           </div>
           <div class="box-header">
-            <form method="GET" action="">
-              <div class="input-group input-group-sm" style="width: 500px;">
-                <input type="text" name="search" class="form-control pull-right" placeholder="Search">
-                <div class="input-group-btn">
-                  <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                </div>
+            <div class="input-group input-group-sm" style="width: 500px;">
+              <input type="text" name="search" class="form-control pull-right" placeholder="Search">
+              <div class="input-group-btn">
+              <button class="btn btn-default"><i class="fa fa-search"></i></button>
               </div>
-            </form>
+            </div>
           </div>
           <!-- /.box-header -->
           <div class="box-body">
             <table id="example2" class="table table-bordered table-hover">
               <thead>
                 <tr>
-                  <th>id</th>
-                  <th>nom</th>
-                  <th>prenom</th>
-                  <th>login</th>
-                  <th>role</th>
-                  <th>action</th>
+                    <th data-column="id" data-order="asc">id</th>
+                    <th data-column="nom" data-order="asc">nom</th>
+                    <th data-column="prenom" data-order="asc">prenom</th>
+                    <th data-column="login" data-order="asc">login</th>
+                    <th data-column="role" data-order="asc">role</th>
+                    <th>action</th>
                 </tr>
               </thead>
               <tbody>
@@ -87,4 +85,77 @@
   <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
+<script>
+$(document).ready(function() {
+  function fetchData(search = '', sort_by = '', sort_order = '') {
+    $.ajax({
+      url: "http://localhost:8000/index.php/utilisateurDashboard/searchUsers",
+      method: "GET",
+      data: { search: search, sort_by: sort_by, sort_order: sort_order },
+      success: function(data) {
+        var users = JSON.parse(data);
+        var tbody = '';
+        var thead = '';
+
+        // Build the table header dynamically
+        thead += '<tr>';
+        thead += '<th data-column="id" data-order="' + (sort_order === 'asc' ? 'desc' : 'asc') + '">id</th>';
+        thead += '<th data-column="nom" data-order="' + (sort_order === 'asc' ? 'desc' : 'asc') + '">nom</th>';
+        thead += '<th data-column="prenom" data-order="' + (sort_order === 'asc' ? 'desc' : 'asc') + '">prenom</th>';
+        thead += '<th data-column="login" data-order="' + (sort_order === 'asc' ? 'desc' : 'asc') + '">login</th>';
+        thead += '<th data-column="role" data-order="' + (sort_order === 'asc' ? 'desc' : 'asc') + '">role</th>';
+        thead += '<th>action</th>';
+        thead += '</tr>';
+        
+        // Update the thead
+        $('thead').html(thead);
+
+        // Generate the tbody based on users data
+        users.utilisateurs.forEach(function(user) {
+          tbody += '<tr>';
+          tbody += '<td>' + user.id + '</td>';
+          tbody += '<td>' + user.nom + '</td>';
+          tbody += '<td>' + user.prenom + '</td>';
+          tbody += '<td>' + user.login + '</td>';
+          tbody += '<td>' + user.role + '</td>';
+          tbody += '<td>';
+          tbody += '<div class="btn-group">';
+          tbody += '<button type="button" class="btn btn-success">Action</button>';
+          tbody += '<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown">';
+          tbody += '<span class="caret"></span>';
+          tbody += '<span class="sr-only">Toggle Dropdown</span>';
+          tbody += '</button>';
+          tbody += '<ul class="dropdown-menu" role="menu">';
+          tbody += '<li><a href="updateUser/' + user.id + '">Modifier</a></li>';
+          tbody += '<li><a href="deleteUser/' + user.id + '">Supprimer</a></li>';
+          tbody += '</ul>';
+          tbody += '</div>';
+          tbody += '</td>';
+          tbody += '</tr>';
+        });
+
+        // Update the tbody
+        $('tbody').html(tbody);
+      }
+    });
+  }
+
+  // Search functionality with debounce
+  $('input[name="search"]').on('input', function() {
+    clearTimeout($.data(this, 'timer'));
+    var search = $(this).val();
+    $(this).data('timer', setTimeout(function() {
+      fetchData(search);
+    }, 1000));
+  });
+
+  // Sorting functionality with event delegation
+  $('table').on('click', 'th', function() {
+    var sort_by = $(this).data('column');
+    var sort_order = $(this).data('order');
+    var search = $('input[name="search"]').val();
+    fetchData(search, sort_by, sort_order);
+  });
+});
+</script>
 <?php $this->load->view('includes/footer'); ?>
